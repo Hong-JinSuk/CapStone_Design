b2fd94900537fd2b4936b1d09d5ef46606df7e92
Hong-JinSuk <86042721+Hong-JinSuk@users.noreply.github.com>
2023-06-02 17:32:27 +0900

    Add files via upload

---
+++ TimeScheduling_algorithm/main.py
@@ -0,0 +1,11 @@
+from 시간분배_알고리즘 import employ_info
+from 시간표_수요조사 import pre_backtrack
+import pandas as pd
+import numpy as np
+
+if __name__=="__main__":
+
+    Dict, Name_list = employ_info() # 직원 정보 집합, 직원 이름 리스트
+    Day_Schedule = pre_backtrack(Dict,Name_list)
+
+    print(Day_Schedule)
