5e2488bca520c1d7d73d7849d82d5dace488b00c
Hong-JinSuk <86042721+Hong-JinSuk@users.noreply.github.com>
2023-06-28 21:27:41 +0900

    6/28 시간표_사전배치 update

--- TimeScheduling_algorithm/main.py
+++ TimeScheduling_algorithm/main.py
@@ -1,11 +1,19 @@
 from 알바생_옵션입력 import employ_info
 from 시간표_수요조사 import pre_backtrack
+from 시간표_사전배치 import pre_place
 import pandas as pd
 import numpy as np
 
 if __name__=="__main__":
 
     Dict, Name_list = employ_info() # 직원 정보 집합, 직원 이름 리스트
-    Day_Schedule = pre_backtrack(Dict,Name_list)
+    # Info = name -> part -> time 순으로 되어있다.
+    # Name_list = name : 주 몇일 으로 되어있다.
+
+    isvisited = [[False]*3]*7 # open,mid,close * 7(월~일)
+    
+    hall_Schedule = pre_backtrack(Dict,Name_list)
+
+    isvisited, hall_Schedule = pre_place(hall_Schedule, isvisited)
 
     print(Day_Schedule)
