6b823e3a5e45a57bde45af9cb90a34d5004981e0
Hong-JinSuk <86042721+Hong-JinSuk@users.noreply.github.com>
2023-11-16 14:44:52 +0900

    Update main.py

--- TimeScheduling_algorithm/main.py
+++ TimeScheduling_algorithm/main.py
@@ -1,6 +1,7 @@
 from 알바생_옵션입력 import employ_info
 from 시간표_수요조사 import pre_backtrack
 from 시간표_사전배치 import pre_place
+from 시간표_자동완성 import random_place
 import pandas as pd
 import numpy as np
 
@@ -16,4 +17,11 @@ if __name__=="__main__":
 
     isvisited, hall_Schedule = pre_place(hall_Schedule, isvisited)
 
-    print(Day_Schedule)
+    hall_Schedule, isvisited = random_place(hall_Schedule, isvisited)
+
+    print(hall_Schedule)
+
+    while True:
+        user_input=input("종료하시겠습니까? (입력 : 1) : ")
+        if user_input=="1":
+            break
